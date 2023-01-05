6a2f2ee61fbe26fd88150bd808f9280d8e35a50c
natolij8585 <anatolij8585@bk.ru>
2023-01-05 17:04:33 +0300

    feat: the brackets algorithmic task is completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let allBrackets = bracketsConfig.join('').replace(/,/g, '');
+  let stack = [];
+    for (let oneBracket of str) {
+      let bracketsInd = allBrackets.indexOf(oneBracket)
+      if (bracketsInd % 2 === 0) {
+        if (oneBracket === allBrackets[bracketsInd + 1] && stack[stack.length - 1] === bracketsInd){
+          stack.pop();
+        } else if (oneBracket === allBrackets[bracketsInd + 1] && stack[stack.length - 1] !== bracketsInd) {
+          stack.push(bracketsInd)
+        } else {
+          stack.push(bracketsInd)
+        }
+      } else {
+          if (stack.pop() !== bracketsInd-1){
+            return false;
+          }
+      }
+    }
+  return stack.length === 0;
 }
